fa1e3f65edd3309c7af1b51243ae26a8de60ea0d
Muhammad Ali <baxtiyorov15@gmail.com>
2023-10-31 20:58:32 +0500

    LoginVC Edit

--- FirstGitHubProject/LoginVC.swift
+++ FirstGitHubProject/LoginVC.swift
@@ -12,7 +12,12 @@ class LoginVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
+        
+        view.backgroundColor = .red
+        
+        
+        
     }
 
 
